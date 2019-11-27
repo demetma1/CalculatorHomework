4d269988dc30b90e80ea470dc8fdbbb21af7e2af
Christine Demetillo <mgd22@njit.edu>
2019-11-27 11:15:51 -0500

    Feature: The subtraction Unit Test is working and reads csv file - Unit Test Subtraction.csv

--- src/Calculator.py
+++ src/Calculator.py
@@ -3,11 +3,15 @@ class Calculator:
     result = 0
 
     def __init__(self):
-        x = 2 + 2
+        x = 4 - 2
         self.result = x;
         pass
 
     def add(self, a, b):
         c = a + b
+        return c
+
+    def subtract(self, a, b):
+        c = b - a
         return c
 
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -10,16 +10,28 @@ class MyTestCase(unittest.TestCase):
 
     def test_results_property_calculator(self):
         calculator = Calculator()
-        self.assertEqual(calculator.result, 4)
+        self.assertEqual(calculator.result, 2)
 
     def test_add_method_calculator(self):
         calculator = Calculator()
-        #self.assertEqual(calculator.add(2,2), 4)
+        self.assertEqual(calculator.add(2, 2), 4)
         data = CsvReader('src/Unit Test Addition.csv').data
         for row in data:
             a = int(row['Value 1'])
             b = int(row['Value 2'])
             c = int(row['Result'])
             self.assertEqual(calculator.add(a, b), c)
+if __name__ == '__main__':
+    unittest.main()
+
+    def test_subtract_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.subtract(2, 4), 2)
+        data = CsvReader('src/Unit Test Subtraction.csv').data
+        for row in data:
+            a = int(row['Value 1'])
+            b = int(row['Value 2'])
+            c = int(row['Result'])
+            self.assertEqual(calculator.subtract(b, a), c)
 if __name__ == '__main__':
     unittest.main()
